243cd2bc86097d4937a3b194eaf2e6be4072bdd2
willmotil <willmotil@users.noreply.github.com>
2021-03-06 23:01:14 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Game_ImguiTestPrj
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Game_ImguiTestPrj
+ ImgUiMonogameExample Using the nuget
